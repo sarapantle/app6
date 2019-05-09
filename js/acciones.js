33c77a964246ec19d5bd38b891e4abbbd466d390
app6 <sarapantle23@gmail.com>
2019-05-09 07:28:56 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -7,7 +7,7 @@ document.addEventListener("deviceready",function(){
       navigator.notification. alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
 });
 
-$('#derecha').on("swipeleft",function(){
+$('#derecha').on("swiperight",function(){
 navigator.notification. confirm("¿Que quieres hacer?",function(opt){
 switch(opt)
 {
